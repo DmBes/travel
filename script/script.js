309ece46aa5286efb44aa9e24f45103b9ba3d1a2
BesDm <b-192@tut.by>
2022-06-27 00:51:12 +0300

    new add

--- script/script.js
+++ script/script.js
@@ -1,1 +1,14 @@
 console.log('hello')
+
+
+var rem =  document.querySelector('.layout-3-column').childNodes;
+
+for (const iterator of rem) {
+    console.log(iterator);
+    if (iterator.nodeName == '#text')
+    {
+        iterator.textContent ='';
+        }  
+    }
+
+
